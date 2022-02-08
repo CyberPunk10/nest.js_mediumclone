d69520208ab961081f990aa7da42dadaec7871c9
Konstantin Ryazanov <konstantin21arx@gmail.com>
2022-02-08 23:36:37 +0300

    3 module, 17 video

--- src/user/user.controllers.ts
+++ src/user/user.controllers.ts
@@ -1,17 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { UserService } from './user.service';
-import { UserEntity } from './user.entity';
+import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import { LoginUserDto } from '@app/user/dto/login.dto';
+import { UserResponseInterface } from './types/userResponce.interface';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
+  @UsePipes(new ValidationPipe())
   async createUser(
     @Body('user') createUserDto: CreateUserDto,
-  ): Promise<{ user: UserEntity }> {
-    const res = await this.userService.createUser(createUserDto);
-    return { user: res };
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponce(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async login(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.login(loginUserDto);
+    return this.userService.buildUserResponce(user);
+  }
+
+  @Get('user')
+  async currentUser(@Req() request: Request): Promise<UserResponseInterface> {
+    console.log('current', request);
+    return 'current' as any;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserEntity } from './user.entity';
+import { sign } from 'jsonwebtoken';
+import { JWT_SECRET } from '@app/config';
+import { UserResponseInterface } from './types/userResponce.interface';
+import { LoginUserDto } from './dto/login.dto';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -12,8 +17,77 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const userByEmail = await this.userRepository.findOne({
+      email: createUserDto.email,
+    });
+    const userByUsername = await this.userRepository.findOne({
+      username: createUserDto.username,
+    });
+
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        'Email or username are taken',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne(
+      {
+        email: loginUserDto.email,
+      },
+      {
+        select: ['id', 'email', 'bio', 'image', 'password', 'username'],
+      },
+    );
+    console.log('user', user);
+
+    if (!user) {
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    const isPasswordCorrest = await compare(
+      loginUserDto.password,
+      user.password,
+    );
+
+    if (!isPasswordCorrest) {
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    delete user.password;
+
+    return user;
+  }
+
+  generateJwt(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      JWT_SECRET,
+    );
+  }
+
+  buildUserResponce(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
   }
 }
